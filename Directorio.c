751c2d3e1f6d13f9b7ed869f3a89d840cc64e532
Tarica <48918032+TaricaTarica@users.noreply.github.com>
2019-11-15 03:36:37 -0300

    Add files via upload
    AHORA SE MUEVE ITER, PUSE VARIOS PRINTS PARA QUE ENTIENDAS

--- Directorio.c
+++ Directorio.c
@@ -35,50 +35,54 @@ directorio create_Raiz(directorio d){
 bool createDirectorio (directorio & d,char * path){
 	directorio aux;
 	directorio iter = d;
+  char * auxPATH = new char [strlen(path) + 1];
+  strcpy(auxPATH,path);
   char * elem = new char [strlen(path) + 1];
   char * elemAUX = new char [strlen(path) + 1];
-  char * papi = new char [strlen(path) + 1];
+  char * pch = new char [strlen(path) + 1];
   elemAUX = strtok (path,"/");
   while (elemAUX != NULL){
     elemAUX = strtok(NULL,"/");
     if(elemAUX != NULL)
       strcpy(elem,elemAUX);
   } //obtengo el nombre del nuevo directorio
-
-  char * pch = strtok (path,"/");
-	while (pch != NULL)// creo q siempre esta tomando valr null despues de la primer pasada
+  printf("DIRECTORIO A INSERTAR: %s\n",elem);
+  pch = strtok (auxPATH,"/");
+  printf("PCH: %s\n", pch);
+	while (pch != NULL)
 	{
-    printf ("\n avance del strtok : %s\n", pch);//cuando sea algo del estilo /1/2/3 deberia imprimir 1 y 2, para meter el 2 en el while siguiente
-    printf("\n iter = %s \n", iter->nombreDirectorio);
-    while (strcmp (pch, iter->nombreDirectorio) != 0 && iter->sig != NULL){//pero imprime solo el primero siempre (raiz)
-        iter = iter->sig;
-    }
+    printf("ENTRO AL WHILE\n");
+		while ((iter->sig != NULL) && (strcmp (pch, iter->nombreDirectorio) != 0))
+				iter = iter->sig;
 		if (iter == NULL)
 			return false;
-    strcpy(papi ,iter->nombreDirectorio);
-    printf ("\n papi: %s", papi);
 		pch = strtok (NULL, "/");
-  }
-  printf("\n elmento = %s", elem);//elemento sale bien, queda el nombre del nuevo subdir
-  printf("\n iter = %s \n", iter->nombreDirectorio);//iter siempre es raiz, osea q siempre va a quedar raiz como padre de los subdir
-  printf ("\n papi: %s", papi);
+    printf("PCH : %s\n", pch);
+		if ((pch != NULL) && (strcmp(elem, pch) != 0)){
+      printf("ENTRO IF");
+		  iter = iter->hijo;
+      printf("ITER ES: %s\n",iter->nombreDirectorio);
+    }
+
+	}
+  printf("AL SALIR PCH ES: %s\n", pch);
+  printf("AL SALIR ITER ES: %s\n",iter->nombreDirectorio);
   aux = new (nodoDirectorio);
 	aux->nombreDirectorio = new char [strlen(elem) + 1];
-
   strcpy (aux->nombreDirectorio, elem);
-	aux->sig = NULL;
-	if (iter->hijo == NULL)
+	aux->hijo = NULL;
+	if (d == NULL)
 	{
-		aux->sig = NULL;//cambie los = porq estaban entreverando cosas
 		aux->hijo = NULL;
+		aux->sig = NULL;
 		aux->padre = NULL;
-		iter->hijo = aux;
+		d = aux;
 	}
 	else
 	{
 		aux->padre = iter;
-		aux->sig = iter->hijo;
 		aux->hijo = NULL;
+		aux->sig = iter->hijo;
 		iter->hijo = aux;
 	}
 	return true;
@@ -108,17 +112,13 @@ void print_Directorio(directorio d){
   if(d != NULL){
     printf("+ %s", d->nombreDirectorio);
     printf("\n");
-    if (d->a != NULL){
-      print_Archivo(d->a);
-    }
+    printf("\t \t");
+    print_Archivo(d->a);
     if (d->hijo != NULL){
       printf("\t \t");
-      printf("\n>>hijo != NULL %s \n", d->nombreDirectorio);//para ver si entra a crear un hijo
       print_Directorio(d->hijo);
     }
     if(d->sig != NULL){
-      printf("\n>> sig != NULL %s", d->nombreDirectorio);//para ver si entra a crear un hermano
-      printf("\n((aca tiene que entrar solo si creas un hermano))\n");
       print_Directorio(d->sig);
     }
   }
@@ -173,12 +173,12 @@ directorio CDdir (directorio d, char*nombreDir){
     while (pch != NULL)
     {
       while ((iter != NULL) && (strcmp (pch, iter->nombreDirectorio) != 0))
-          iter = iter->hijo;
+          iter = iter->sig;
       if (iter == NULL)
         return d;
       pch = strtok (NULL, "/");
       if (pch != NULL)
-        iter = iter->sig;
+        iter = iter->hijo;
     }
     return iter;
   }
@@ -206,12 +206,11 @@ void PWDir (directorio d, char * nombreDirectorio){
   printf("%s/", get_nDirectorio(d));
   while (d != NULL)
   {
-    while ((d->hijo != NULL) && (strcmp (nombreDirectorio, d->nombreDirectorio) != 0)){
-        printf("ENTRO SEGUNDO WHILE");
-        d = d->hijo;
+    while ((d->sig != NULL) && (strcmp (nombreDirectorio, d->nombreDirectorio) != 0)){
+        d = d->sig;
         printf("%s/", get_nDirectorio(d));
     }
-    d = d->sig;
+    d = d->hijo;
   }
     if(strcmp("raiz",nombreDirectorio) != 0)
       printf("%s/", nombreDirectorio);
